cead943ef25970f5bb6433b164ee3808747c590d
stasiekq <staszek5286@gmail.com>
2023-12-28 00:10:09 +0100

    internactionalization finished

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,37 +1,49 @@
-// scripts.js
 document.addEventListener('DOMContentLoaded', function () {
-    const darkModeToggle = document.getElementById('dark-mode-toggle');
-    const body = document.body;
-  
-    // Sprawdź, czy tryb ciemny jest zapisany w localStorage
-    const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
-  
-    // Jeśli tryb ciemny jest zapisany, włącz go na starcie
-    if (isDarkMode) {
+  const darkModeToggle = document.getElementById('dark-mode-toggle');
+  const body = document.body;
+
+  // Sprawdź, czy tryb ciemny jest zapisany w localStorage
+  const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+
+  // Jeśli tryb ciemny jest zapisany, włącz go na starcie
+  if (isDarkMode) {
       body.classList.add('dark-mode');
       darkModeToggle.checked = true;
-    }
-  
-    // Obsługa zmiany trybu jasnego/ciemnego
-    darkModeToggle.addEventListener('change', function () {
+  }
+
+  // Obsługa zmiany trybu jasnego/ciemnego
+  darkModeToggle.addEventListener('change', function () {
       if (darkModeToggle.checked) {
-        body.classList.add('dark-mode');
-        localStorage.setItem('darkMode', 'enabled');
+          body.classList.add('dark-mode');
+          localStorage.setItem('darkMode', 'enabled');
       } else {
-        body.classList.remove('dark-mode');
-        localStorage.setItem('darkMode', null);
+          body.classList.remove('dark-mode');
+          localStorage.setItem('darkMode', null);
       }
-    });
-});
-  
+  });
 
-document.addEventListener('DOMContentLoaded', function () {
-  var translateElements = document.querySelectorAll('.translate');
-  var langButtons = document.querySelectorAll('.lang-switch');
+  // Obsługa zmiany języka
+  const langButtons = document.querySelectorAll('.lang-switch');
+  const translateElements = document.querySelectorAll('.translate');
 
+  // Sprawdź, czy język jest zapisany w localStorage
+  const savedLang = localStorage.getItem('preferredLang');
+
+  // Jeśli język jest zapisany, ustaw go
+  if (savedLang) {
+      translateElements.forEach(function (element) {
+          element.textContent = element.dataset['lang' + savedLang];
+      });
+  }
+
+  // Dodaj obsługę zmiany języka
   langButtons.forEach(function (button) {
       button.addEventListener('click', function () {
           var lang = this.dataset.lang;
+
+          // Zapisz wybrany język do localStorage
+          localStorage.setItem('preferredLang', lang);
+
           translateElements.forEach(function (element) {
               element.textContent = element.dataset['lang' + lang];
           });
--- static/js/translate.js
+++ static/js/translate.js
@@ -1,10 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
     var translateElements = document.querySelectorAll('.translate');
     var langButtons = document.querySelectorAll('.lang-switch');
+    
+    // Sprawdzanie, czy zapisany język istnieje w localStorage
+    var savedLang = localStorage.getItem('preferredLang');
+    
+    // Jeśli język jest zapisany, ustaw go
+    if (savedLang) {
+        translateElements.forEach(function (element) {
+            element.textContent = element.dataset['lang' + savedLang];
+        });
+    }
 
     langButtons.forEach(function (button) {
         button.addEventListener('click', function () {
             var lang = this.dataset.lang;
+            
+            // Zapisz wybrany język do localStorage
+            localStorage.setItem('preferredLang', lang);
+
             translateElements.forEach(function (element) {
                 element.textContent = element.dataset['lang' + lang];
             });
